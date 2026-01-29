e40e33977b76d129a7e6b48de7937474600a4478
Forinarr <shuvalova0723@list.ru>
2026-01-29 18:28:22 +0300

    added fio and hi

--- server/Program.cs
+++ server/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Здравствуйте! Добро пожаловать в программу.");
+        string fio = "Shuvalova Alina";
+    }
+}
